1a31ceae6e162944535d957b6eb49298a5900417
Frankie <frankie@frankie-NV55C.(none)>
2012-12-01 00:54:10 -0500

    PyTexting v0.1

--- pytexting.py
+++ pytexting.py
@@ -16,5 +16,7 @@ class pytexting:
    "Virgin":"@vmobl.com",
 }
 
-  def send(self, phone_num, message)
-    
+  def send(self, phone_num, message, carrier):
+    phone_num = phone_num + self.carriers[carrier]
+    message = (" \r\n\r\n From: %s\r\n To: %s\r\n\r\n  %s" % (self.email, "" .join(phone_num), "" .join(message)))
+    self.smtp.sendmail(self.email, phone_num, message)
